583d956dd0bcc462d87a35e7241caa09730ba9b3
manchenlee <sleep1700505@gmail.com>
2022-04-24 22:19:54 +0800

    add df11

--- df11.py
+++ df11.py
@@ -1,25 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 import time, re
+import csv
 import numpy as np
 import pandas as pd
 
 # data 讀檔
-d1 = np.array( pd.read_csv('R&B_靈魂_藍調_爵士_拉丁.csv', header=None).transpose() )[0]
-d2 = np.array( pd.read_csv('舞曲_Remix_ACG_電音.csv', header=None).transpose() )[0]
-d3 = np.array( pd.read_csv('嘻哈_饒舌_雷鬼_Funk.csv', header=None).transpose() )[0]
+d9 = pd.read_csv('df9-count.csv', header=None)
+data = d9[0].values.tolist()
 
-data = np.concatenate((d1, d2, d3))
-data = np.unique(data)
-
-def LikesCrawler(uid):
+def LikesCrawler(uid, writer):
     url = "https://streetvoice.com" + uid + "likes"
     #print(url)
-    driver = webdriver.Chrome(executable_path="chromedriver.exe")
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(
+        executable_path='chromedriver.exe', options=chrome_options)
     driver.maximize_window()
     driver.set_page_load_timeout(600)
     driver.get(url)
@@ -33,7 +34,7 @@ def LikesCrawler(uid):
     last_height = driver.execute_script("return document.body.scrollHeight")
     while True:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(30)
+        time.sleep(1)
         new_height = driver.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
             break
@@ -51,10 +52,8 @@ def LikesCrawler(uid):
             temp = a['href']
             inf = temp.split('/')
             #print(inf)
-            Uid.append(uid[1:-1])
-            Song.append(inf[3])
-            ArtistUid.append(inf[1])
-            
+            writer.writerow([uid[1:-1], inf[3], inf[1]])
+'''           
 def CollabCrawler(uid):
     url = "https://streetvoice.com" + uid + "songs"
     #print(url)
@@ -87,27 +86,34 @@ Uid = []
 Song = []
 ArtistUid = []
 CollabUid = []
-
+'''
 # 如果發生 Exception，shell 將會 print 出當前執行的 index 
 # 若程式因 Exception 停止，請執行以下步驟：
 # 1. 把 count 和 data[<count>:] 的數值改成 index
 # 2. 把 df11.csv 的內容存到另一個 csv 檔中
 # 3. 再次啟動程式
-count = 0
-for uid in data[0:]:
-    try:
-        LikesCrawler(uid)
-        count += 1
-    except:
-        print(count)
-        print("======== df11 saving ... ========")
-        df11 = pd.DataFrame(zip(Uid, Song, ArtistUid), columns = ['Uid', 'Liked Song', 'ArtistUid'])
-        df11.to_csv('df11.csv', index=False)
-        
-        time.sleep(30)
-        LikesCrawler(uid)
-        count += 1
+data_01 = data[0:400]
+data_02 = data[400:800]
+data_03 = data[800:1200]
+data_04 = data[1200:1600]
+data_05 = data[1600:2000]
+data_06 = data[2000:2400]
+data_07 = data[2400:2800]
+data_08 = data[2800:3200]
+data_09 = data[3200:3600]
+data_10 = data[3600:]
+
+with open('df11_8.csv', 'w', newline='', encoding="utf8") as csvfile:
+    writer = csv.writer(csvfile)
+    # 如果發生 Exception，會 print 出當前執行的 index (count)
+    count = 0
+    for d in data_08:
+        try:
+            LikesCrawler(d, writer)
+            count += 1
+        except:
+            print(count)
+            time.sleep(30)
+            LikesCrawler(d, writer)
+            count += 1
   
-print("======== df11 complete ========")
-df11 = pd.DataFrame(zip(Uid, Song, ArtistUid), columns = ['Uid', 'Liked Song', 'ArtistUid'])
-df11.to_csv('df11.csv', index=False)
